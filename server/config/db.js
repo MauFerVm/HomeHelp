c2b05a23dee5c63a17e83d7bf185ba5a97acd798
MauFerVm <maufer.vm@gmail.com>
2025-11-17 14:11:27 -0300

    Agregado scroll a ciertas formularios que no tenian

--- server/config/db.js
+++ server/config/db.js
@@ -4,9 +4,9 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: 'localhost',
-  user: 'vale',
-  password: '1234',
-  database: 'bd_home_help1',
+  user: 'root',
+  password: '',
+  database: 'home_help',
 });
 
 // Exportamos el pool con promesa integrada
